f76b2dc496e41e9014d1d26151f41fc8ed67d8fc
wanyi <408629610@qq.com>
2022-04-23 14:48:11 +0000

    完成基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="http://whispering-brook-20656.herokuapp.com/" target="_blank">Wanyi</a> 设计和编码<span style="color: #e27575; font-size: 14px;"> ❤</span>
+
+      <a href="#">联系我们</a>
+    </p>
+
+
+  </div>
+</footer>
