1bd87759c63f042a741650846690a65cd7bcba2f
wanyi <408629610@qq.com>
2022-04-24 17:03:20 +0000

    密码重置

--- routes/web.php
+++ routes/web.php
@@ -30,9 +30,9 @@ Route::post('register','Auth\RegisterController@register');
 
 //密码重置相关路由
 Route::get('password/reset','Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/email','Auth\ForgotPasswordController@sendLinkEmail')->name('password.email');
-Route::get('password/reset/{token}','Auth\ForgotPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset','Auth\ForgotPasswordController@reset')->name('password.update');
+Route::post('password/email','Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}','Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset','Auth\ResetPasswordController@reset')->name('password.update');
 
 //Email 认证相关路由
 Route::get('email/vrify','Auth\VerificationController@show')->name('verification.notice');
