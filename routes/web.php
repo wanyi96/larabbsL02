908f476f42de4b119c265701c49d2ad5d9dfc283
wanyi <408629610@qq.com>
2022-04-24 05:13:57 +0000

    生成用户认证代码，加载中文语言包

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,26 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 Route::get('/','PagesController@root')->name('root');
+
+// Auth::routes();
+//用户身份证验证相关的路由
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login','Auth\LoginController@login');
+Route::post('loginout','Auth\LoginController@login');
+
+//用户注册相关路由
+Route::get('register','Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register','Auth\LoginController@register');
+
+//密码重置相关路由
+Route::get('password/reset','Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email','Auth\ForgotPasswordController@sendLinkEmail')->name('password.email');
+Route::get('password/reset/{token}','Auth\ForgotPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset','Auth\ForgotPasswordController@reset')->name('password.update');
+
+//Email 认证相关路由
+Route::get('email/vrify','Auth\VerificationController@show')->name('verification.notice');
+Route::get('email/vrify/{id}/{hash}','Auth\VerificationController@verify')->name('verification.verify');
+Route::get('email/resend','Auth\VerificationController@resend')->name('verification.resend');
+
+
