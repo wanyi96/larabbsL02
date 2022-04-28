baba4455448bc39a53ea741cb2f89f3494797bda
wanyi <408629610@qq.com>
2022-04-28 14:33:14 +0000

    填充用户和话题数据

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -11,8 +11,16 @@ class TopicFactory extends Factory
 
     public function definition()
     {
+        //sentence()方法会随机生成小段落文本
+        $sentence  = $this->faker->sentence();
+
         return [
             // $this->faker->name,
+            'title' => $sentence,
+            'body' => $this->faker->text(), //text()会随机生成大段落文本
+            'excerpt' => $sentence,
+            'user_id' =>$this->faker->randomElement([1,2,3,4,5,6,7,8,9,10]),
+            'category_id' => $this->faker->randomElement([1,2,3,4]),
         ];
     }
 }
