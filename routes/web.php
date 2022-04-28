0f9884c0d28e5236799fe2361c099634d10558d5
wanyi <408629610@qq.com>
2022-04-28 14:00:26 +0000

    使用代码生成器生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,5 @@ Route::post('email/resend','Auth\VerificationController@resend')->name('verifica
 Route::get('/users/{user}','UsersController@show')->name('users.show');
 Route::get('/users/{user}/edit','UsersController@edit')->name('users.edit');
 Route::put('/users/{user}','UsersController@update')->name('users.update');
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
