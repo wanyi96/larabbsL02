248eef1b570811d040a8a64c0c95d98831d81225
wanyi <408629610@qq.com>
2022-05-07 12:19:57 +0000

    xss安全漏洞修复

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -21,6 +21,9 @@ class TopicObserver
 
     public function saving(Topic $topic)
     {
+        //过滤发帖的数据
+        $topic->body = clean($topic->body, 'user_topic_body');
+
         //保存topic数据时，生成摘要字段再保存
         $topic->excerpt = make_excerpt($topic->body);
     }
