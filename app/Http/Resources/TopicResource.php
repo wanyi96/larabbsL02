3ec5d7453aa550164836ba1449f93a649381c85f
wanyi <408629610@qq.com>
2022-05-25 16:02:57 +0000

    话题列表

--- app/Http/Resources/TopicResource.php
+++ app/Http/Resources/TopicResource.php
@@ -22,6 +22,9 @@ class TopicResource extends JsonResource
             'slug' => $this->slug,
             'created_at' => (string) $this->created_at,
             'updated_at' => (string) $this->updated_at,
+            //通过 whenLoaded 判断是否已经预加载了 user 和 category;如果有，则使用对应的 Resource 处理并返回数据。
+            'user' => new UserResource($this->whenLoaded('user')),
+            'category' => new CategoryResource($this->whenLoaded('category')),
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -63,6 +63,9 @@ Route::prefix('v1')
                 Route::resource('topics', 'TopicsController')->only([
                     'index', 'show'
                 ]);
+                // 某个用户发布的话题
+                Route::get('users/{user}/topics', 'TopicsController@userIndex')
+                    ->name('users.topics.index');
 
                 // 登录后可以访问的接口
                 Route::middleware('auth:api')->group(function() {
