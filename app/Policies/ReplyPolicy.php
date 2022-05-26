c248f47437930ea34e7457014266f868146cac64
wanyi <408629610@qq.com>
2022-05-26 06:33:59 +0000

    用户权限列表

--- app/Policies/ReplyPolicy.php
+++ app/Policies/ReplyPolicy.php
@@ -19,4 +19,12 @@ class ReplyPolicy extends Policy
         //拥有回复删除权限的只能是回复的作者或者是话题的作者
         return $user->isAuthorOf($reply) || $user->isAuthorOf($reply->topic);
     }
+
+    public function before($user, $ability)
+    {
+        // 如果用户拥有管理内容的权限的话，即授权通过
+        if ($user->can('manage_contents')) {
+            return true;
+        }
+    }
 }
