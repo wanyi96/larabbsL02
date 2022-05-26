e51783e63c086ce455f89bde16fe21a9ac2283db
wanyi <408629610@qq.com>
2022-05-26 00:21:10 +0000

    删除回复

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -14,9 +14,21 @@ class RepliesController extends Controller
     {
         $reply->content = $request->content;
         $reply->topic()->associate($topic);  //associate()将模型实例与给定的父对象关联。
-        $reply->user()->associate($request->user());
+        $reply->user()->associate($request->user()); //个人猜测相当于$reply->user() = $request->user()
         $reply->save();
 
         return new ReplyResource($reply);
+    }
+
+    public function destroy(Topic $topic, Reply $reply)
+    {
+        if ($reply->topic_id != $topic->id) {
+            abort(404);
+        }
+
+        $this->authorize('destroy', $reply);
+        $reply->delete();
+
+        return response(null, 204);
     }
 }
